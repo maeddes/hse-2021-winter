449c27d7ca0b390ff3d6b6c373c3975ab33d96ab
Matthias Haeussler <maeddes@gmail.com>
2021-10-18 11:57:01 +0200

    20211018 Docker part

--- 20211011/HelloREST/src/main/java/com/example/HelloREST/HelloRestApplication.java
+++ 20211011/HelloREST/src/main/java/com/example/HelloREST/HelloRestApplication.java
@@ -1,5 +1,7 @@
 package com.example.HelloREST;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,22 +14,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRestApplication {
 
+	private Logger logger = LoggerFactory.getLogger(HelloRestApplication.class);
+
 	@Value("${my.property:unset}")
 	String internalProperty;
 
 	@GetMapping("/hello")
 	public String sayHello(){
 
-		return "Guten Morgen, Esslingen "+internalProperty;
+		//logger.info("Current internal Property {}", internalProperty);
+		logger.warn("Internal Property {}", internalProperty);
+		System.out.println("In method hello. Internal Property = " + this.internalProperty);
+		return "Guten Morgen, Esslingen "+ internalProperty;
 		
 	}
 
 	@PostMapping("/setProperty/{paramProperty}")
 	public String setProporty(@PathVariable String paramProperty){
 
+		logger.info("Old property {} New Property {} ", internalProperty, paramProperty);	
 		internalProperty = paramProperty;
+		logger.info("");
+		System.out.println(" In set Property. New internal property: "+internalProperty);
 		return internalProperty;
 	}
+
+	//@PutMapping
+	//@DeleteMapping
+	//@RequestMapping(method = POST, )
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloRestApplication.class, args);
